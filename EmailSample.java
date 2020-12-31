0487c04ae9f57e42b832df074dc4e9f0f84c3fbd
Tejeswar0475 <tejeswar096@gmail.com>
2020-12-31 16:46:04 +0530

    Added code forr email sample2

--- EmailSample.java
+++ EmailSample.java
@@ -6,14 +6,24 @@ public class EmailSample {
 
 	public static void main(String[] args)
 	{
+		//UC1
 		System.out.println("Welcome to Email Samples");
-		Pattern pattern=Pattern.compile("[a-z]{3,}@[a-z]{5,}.com");
+		Pattern pattern=Pattern.compile("[a-z]{3,}@[a-z]{5,}.[a-z]{3,}$");
 		Matcher match1=pattern.matcher("abc@yahoo.com");
 		Matcher match2=pattern.matcher("tejeswar@yahoo.com");
 		boolean email1=match1.matches();
-		System.out.println("Email1: "+email1);
+		System.out.println("UC1 Email1: "+email1);
 		boolean email2=match2.matches();
-		System.out.println("Email2: "+email2);
-	}
+		System.out.println("UC1 Email2: "+email2);
 
+		//UC2
+		Pattern patternUC2=Pattern.compile("[a-z]{3,}(-{1})[0-9]{1,}@[a-z]{5,}.[a-z]{3,}");
+		Matcher matchUC2=patternUC2.matcher("abc-100@yahoo.com");
+		boolean emailUC2=matchUC2.matches();
+		System.out.println("UC2 Email1: "+emailUC2);
+
+		Matcher matchUC22=patternUC2.matcher("tejeswar-12782@yahoo.com");
+		boolean emailUC22=matchUC22.matches();
+		System.out.println("UC2 Email2: "+emailUC22);
+	}
 }
